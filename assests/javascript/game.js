a3b3fc9459411ae33b2dfebfba94e14a6c3410de
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-03-11 17:29:07 -0700

    adding work

---
+++ assests/javascript/game.js
@@ -0,0 +1,132 @@
+// create varibales for game (wordbank, wins, losses, picked word, guesses left, game running, picked word placeholder, guessed letter bank, incorrect letter bank)
+
+var wordBank = ["United States", "Canada", "Australia", "Indonesia", "Thailand", "Cambodia", "Mexico"];
+var wins = 0;
+var losses = 0;
+var pickedWord = " ";
+var guessesLeft = 12;
+var gameRunning = false;
+var pickedWordPlaceHolderArray = [];
+var guessedLetterBank = [];
+var incorrectLetterBank = [];
+
+// Call all DOM Elements to write to the index
+
+var domNewGameButton = document.getElementById("new-game-button");
+var domPlaceHolders = document.getElementById("placeholders");
+var domGuessedLetters = document.getElementById("guessed-letters");
+var domGuessesLeft = document.getElementById("guesses-left");
+var domWins = document.getElementById("wins");
+var domLosses = document.getElementById("losses");
+
+// Game running is set to false above because I want to use a new game button that resets the guessed word and guessed missed
+
+// Run game 
+
+function newGame() {
+
+    gameRunning = true;
+    guessLeft =12;
+    pickedWordPlaceHolderArray = [];
+    guessedLetterBank = [];
+    incorrectLetterBank = [];
+
+    // computer picking a word with Math
+
+    pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    // Now for loop to create the underscores for the word length and a - for the space in one of the words
+
+    for (var i = 0; i < pickedWord.length; i++) {
+        if (pickedWord[i] === " ") {
+            pickedWordPlaceHolderArray.push("-");
+
+        }
+        else {
+            pickedWordPlaceHolderArray.push("_");
+        }
+
+
+    }
+
+    // varables that will be added to the dom
+    domGuessesLeft.textContent = guessesLeft;
+    domPlaceHolders.textContent = pickedWordPlaceHolderArray.join(" ");
+    domGuessedLetters.textContent = guessedLetterBank;
+
+}
+
+function letterGuess(letter) {
+    console.log(letterGuess);
+    // running game inside this funtion
+    
+    if (gameRunning === true && guessLetterBank.indexof(letter) === -1) {
+        guessedLetterBank.push(letter);
+        
+    //for loop checking if guessed letter is a lowercased letter and if it matches picked word
+    for (var i = 0; i < pickedWord.length; i++) {
+        if (pickedWord[i].toLowerCase() === letter.toLowerCase()) {
+            pickedWordPlaceHolderArray[i] = pickedWord[i];
+        }     
+    }
+    
+    domPlaceHolders.textContent = pickedWordPlaceHolderArray.join("  ");
+    checkIfCorrect(letter);
+    }
+
+    else{
+        alert("You have already guessed that letter or Click button to start new game");
+    }
+}
+
+function checkIfCorrect(letter) {
+    if (pickedWordPlaceHolderArray.indexOf(letter.toLowerCase())=== -1 &&
+    pickedWordPlaceHolderArray.indexOf(letter.toUpperCase())=== -1){
+
+        guessesLeft --;
+        incorrectLetterBank.push(letter);
+        domGuessedLetters.textContent = incorrectLetterBank.join(" ");
+        domGuessesLeft.textContent = guessesLeft;
+    }
+    checkLoss();
+}
+
+function checkLoss(){
+    if (guessLeft === 0) {
+        losses++;
+        gameRunning = false; 
+        domLosses.textContent = losses;
+        domPlaceHolders.textContent = pickedWord;
+    }
+    checkWin();
+
+}
+
+function checkWin() {
+    if (pickedWord.toLowerCase() === pickedPlaceHolderArray.join("").toLowerCase()) {
+        wins++;
+        gameRunning = false;
+        domWins.textContent = wins;
+        
+    }
+    
+}
+
+
+// Adding event listener to start new game on button
+
+// domNewGameButton.addEventListener("click", newGame);
+
+domNewGameButton.addEventListener("click", newGame);
+
+// domNewGameButton.onClick = newGame;
+
+// adding on key up event for computer to see what keys are pushed
+
+document.onkeyup = function (event){
+    if (event.keyCode >=65 && event.keycode <=90){
+        letterGuess(event.key);
+    } 
+           
+}
+
